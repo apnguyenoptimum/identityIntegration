742e5d2724e150cd181ba64c8c153019c83449ec
Prastut Pandey <prpandey@optimumcs.com>
2021-11-29 16:42:40 -0600

    API response casing issue fixed

--- DecisionFlow/Startup.cs
+++ DecisionFlow/Startup.cs
@@ -11,6 +11,12 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Unchase.Swashbuckle.AspNetCore.Extensions.Extensions;
+using Unchase.Swashbuckle.AspNetCore.Extensions.Options;
 
 namespace DecisionFlow
 {
@@ -29,24 +35,128 @@ namespace DecisionFlow
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo()
+                {
+                    Title = "Decision Flow API",
+                    Version = "v1"
+                });
+            });
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
-                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+              .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
 
             services.AddAuthentication()
-                .AddIdentityServerJwt();
-            services.AddControllersWithViews().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+               .AddIdentityServerJwt();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
+           // services.AddControllersWithViews().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+            services.AddControllersWithViews()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+                });
             services.AddRazorPages();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    // Include XML Comments
+            //    var filePath = Path.Combine(System.AppContext.BaseDirectory, "DF.xml");
+            //    c.IncludeXmlComments(filePath);
+
+            //    // Add x-api-key authentication to the tryitout
+            //    c.AddSecurityDefinition("ApiKey", new OpenApiSecurityScheme()
+            //    {
+            //        Name = "x-api-key",
+            //        In = ParameterLocation.Header,
+            //        Type = SecuritySchemeType.ApiKey,
+            //        Description = "Authorization by x-api-key inside request's header",
+            //        Scheme = "ApiKeyScheme"
+            //    });
+
+            //    var key = new OpenApiSecurityScheme()
+            //    {
+            //        Reference = new OpenApiReference
+            //        {
+            //            Type = ReferenceType.SecurityScheme,
+            //            Id = "ApiKey"
+            //        },
+            //        In = ParameterLocation.Header
+            //    };
+
+            //    var requirement = new OpenApiSecurityRequirement
+            //    {
+            //       { key, new List<string>() }
+            //    };
+            //    c.AddSecurityRequirement(requirement);
+
+            //    c.AddEnumsWithValuesFixFilters(services, o =>
+            //    {
+            //        // add schema filter to fix enums (add 'x-enumNames' for NSwag) in schema
+            //        o.ApplySchemaFilter = true;
+
+            //        // add parameter filter to fix enums (add 'x-enumNames' for NSwag) in schema parameters
+            //        o.ApplyParameterFilter = true;
+
+            //        // add document filter to fix enums displaying in swagger document
+            //        o.ApplyDocumentFilter = true;
+
+            //        // add descriptions from DescriptionAttribute or xml-comments to fix enums (add 'x-enumDescriptions' for schema extensions) for applied filters
+            //        o.IncludeDescriptions = true;
+
+            //        // add remarks for descriptions from xml-comments
+            //        o.IncludeXEnumRemarks = true;
+
+            //        // get descriptions from DescriptionAttribute then from xml-comments
+            //        o.DescriptionSource = DescriptionSources.DescriptionAttributesThenXmlComments;
+
+            //        // get descriptions from xml-file comments on the specified path
+            //        // should use "options.IncludeXmlComments(xmlFilePath);" before
+            //        o.IncludeXmlCommentsFrom(filePath);
+            //        // the same for another xml-files...
+            //    });
+            //});
             services.AddDecisionFlowDataProviderServiceCollection();
         }
 
@@ -72,6 +182,15 @@ namespace DecisionFlow
                 app.UseSpaStaticFiles();
             }
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DecisionFlow API V1");
+            });
             app.UseRouting();
 
             app.UseAuthentication();
