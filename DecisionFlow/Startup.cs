7f9d377f1937c70aae14d1e7878cedb6a295de19
Prastut Pandey <prpandey@optimumcs.com>
2021-11-29 09:22:19 -0600

    Added API controllers, and some supporting projects

--- DecisionFlow/Startup.cs
+++ DecisionFlow/Startup.cs
@@ -47,6 +47,7 @@ namespace DecisionFlow
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+            services.AddDecisionFlowDataProviderServiceCollection();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
